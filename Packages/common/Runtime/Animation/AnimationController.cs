cbb6238a2a8552acebda61a1e187a8a6984143b0
Yu5h1 <jenyu5h10415@gmail.com>
2025-10-23 16:47:28 +0800

    update

--- Packages/common/Runtime/Animation/AnimationController.cs
+++ Packages/common/Runtime/Animation/AnimationController.cs
@@ -5,5 +5,18 @@ using Yu5h1Lib;
 
 public class AnimationController : ComponentController<Animator>
 {
- 
+    public Animator animator => component;
+
+    public void SetLayerActive(string layerName,bool active)
+    {
+        if (animator == null || !isActiveAndEnabled)
+            return;
+        var layerIndex = animator.GetLayerIndex(layerName);
+        if (layerIndex < 0)
+            return;
+        animator.SetLayerWeight(layerIndex, active ? 1f : 0f);
+    }
+
+    public void ActivateLayer(string layerName) => SetLayerActive(layerName, true);
+    public void DeactivateLayer(string layerName) => SetLayerActive(layerName, false);
 }
